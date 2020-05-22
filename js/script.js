cd0e52c3dfd7b891d813cb5d8112d7e5c80cdee8
oleoot <oleoot28@gmail.com>
2020-05-22 14:39:14 +0300

    Drag and drop functions

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@ fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
 
+// Loop through empties and call drag events
+empties.forEach((empty) => {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+})
+
+
 // Drag functions
 function dragStart() {
     this.className += ' hold';
@@ -15,5 +24,23 @@ function dragStart() {
 
 
 function dragEnd() {
+    this.className = "fill"
+}
 
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'empty'
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
 }
